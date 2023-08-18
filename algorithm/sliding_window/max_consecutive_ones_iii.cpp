162039011ba8211e88f286feb8052c0353ca4499
lidongze91 <lidongze91@gmail.com>
2023-08-18 11:06:53 -0700

    sliding window

---
+++ algorithm/sliding_window/max_consecutive_ones_iii.cpp
@@ -0,0 +1,65 @@
+// https://leetcode.com/problems/max-consecutive-ones-iii
+
+/*
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Ex1:
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Ex2:
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+*/
+
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+int longestOnes(vector<int>& nums, int k) {
+    if (nums.empty()) {
+        return 0;
+    }
+
+    int n = nums.size();
+    int l = 0, r = 0;
+    int cnt = 0;
+    int res = 0;
+
+    while (r < n) {
+        if (nums[r] == 1) {
+            cnt++;
+            r++;
+        } else {
+            if (k > 0) {
+                cnt++;
+                k--;
+                r++;
+            } else {
+                if (nums[l] == 0) {
+                    k++;
+                }
+                cnt--;
+                l++;
+            }
+        }
+
+        res = max(res, cnt);
+    }
+
+    return res;
+}
+
+int main() {
+    vector<int> nums1 = {1,1,1,0,0,0,1,1,1,1,0};
+    assert(longestOnes(nums1, 2) == 6);
+
+    vector<int> nums2 = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+    assert(longestOnes(nums2, 3) == 10);
+}
---
+++ algorithm/sliding_window/max_vowels_substring.cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length
+
+/*
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+Ex1:
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+
+Ex2:
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+
+Ex3:
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+*/
+
+#include <string>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+bool isVowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+// Time complexity: O(n), Space complexity: O(1)
+int maxVowels(string s, int k) {
+    if (s.empty()) {
+        return 0;
+    }
+
+    int n = s.size();
+    int l = 0, r = 0;
+    int res = 0;
+    int cnt = 0;
+
+    while (r < n) {
+        if (isVowel(s[r])) {
+            cnt++;
+            res = max(res, cnt);
+        }
+
+        if (r-l+1 == k) {
+            if (isVowel(s[l])) {
+                cnt--;
+            }
+            l++;
+        }
+
+        r++;
+    }
+
+    return res;
+}
+
+int main() {
+    assert(maxVowels("abciiidef", 3) == 3);
+    assert(maxVowels("aeiou", 2) == 2);
+    assert(maxVowels("leetcode", 3) == 2);
+}
