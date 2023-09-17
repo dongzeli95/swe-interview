8352c86c8be399b5d7e5cb0c45d30df14c09683e
lidongze91 <lidongze91@gmail.com>
2023-09-17 11:51:45 -0700

    Union find

--- algorithm/union_find/accounts_merge.cpp
+++ algorithm/union_find/accounts_merge.cpp
@@ -28,10 +28,80 @@ Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <cassert>
 #include <iostream>
 
 using namespace std;
+
+class UnionFind {
+public:
+    // Amortized O(1)
+    string find(string curr) {
+        if (!parents.count(curr)) return curr;
+        if (parents[curr] == curr) {
+            return curr;
+        }
+
+        parents[curr] = find(parents[curr]);
+        return parents[curr];
+    }
+
+    // Amortized O(1)
+    void uni(string i, string j) {
+        string p1 = find(i);
+        string p2 = find(j);
+        if (p1 != p2) {
+            int s1 = sizes[p1];
+            int s2 = sizes[p2];
+            sizes[p2] = s1+s2;
+            parents[p1] = p2;
+        }
+    }
+
+private:
+    unordered_map<string, string> parents;
+    unordered_map<string, int> sizes;
+};
+
+// N accounts, each account have K emails
+// Time: O(NK * Log(NK)), Space: O(NK)
+vector<vector<string>> accountsMergeUF(vector<vector<string>>& accounts) {
+    unordered_map<string, string> owner;
+    int n = accounts.size();
+    UnionFind uf = UnionFind();
+    for (int i = 0; i < n; i++) {
+        string name = accounts[i][0];
+        int s = accounts[i].size();
+        string email1 = accounts[i][1];
+        owner[email1] = name;
+        for (int j = 2; j < s; j++) {
+            string e = accounts[i][j];
+            uf.uni(email1, e);
+            owner[e] = name;
+        }
+    }
+
+    unordered_map<string, set<string>> clusters;
+    for (auto i : owner) {
+        string name = i.second;
+        string email = i.first;
+        string p = uf.find(email);
+        clusters[p].insert(p);
+        clusters[p].insert(email);
+    }
+
+    vector<vector<string>> res;
+    for (auto i : clusters) {
+        string p = i.first;
+        string name = owner[p];
+        vector<string> account = {name};
+        copy(clusters[p].begin(), clusters[p].end(), back_inserter(account));
+        res.push_back(account);
+    }
+
+    return res;
+}
 
 void dfs(unordered_map<string, vector<string>>& graph,
         unordered_set<string>& visited,
@@ -88,7 +158,7 @@ int main() {
                                         {"John","johnsmith@mail.com", "john00@mail.com"},
                                         {"Mary","mary@mail.com"},
                                         {"John", "johnnybravo@mail.com"} };
-    vector<vector<string>> result1 = accountsMergeDFS(accounts1);
+    vector<vector<string>> result1 = accountsMergeUF(accounts1);
     assert(result1.size() == 3);
     for (int i = 0; i < result1.size(); i++) {
         for (int j = 0; j < result1[i].size(); j++) {
@@ -103,7 +173,7 @@ int main() {
                                         {"Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"},
                                         {"Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"},
                                         {"Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"} };
-    vector<vector<string>> result2 = accountsMergeDFS(accounts2);
+    vector<vector<string>> result2 = accountsMergeUF(accounts2);
     for (int i = 0; i < result2.size(); i++) {
         for (int j = 0; j < result2[i].size(); j++) {
             cout << result2[i][j] << " ";
